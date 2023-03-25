3ddb4678c726fcff8f0c2fe96833b8cb06e119a9
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-03-25 14:49:25 +0200

    Updated file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pickle
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash
 from Monthly_Revenue_Forecast import preprocess_data, predict_next_month
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -12,6 +14,14 @@ url = "https://github.com/aurimas13/Revenue-Forecast/blob/main/Dataset/Barbora%2
 
 revenue_data = pd.read_excel(url, sheet_name='Revenue', parse_dates=['Date'], engine='openpyxl')
 weather_data = pd.read_excel(url, sheet_name='Weather', parse_dates=['dt'], engine='openpyxl')
+
+UPLOAD_FOLDER = 'Dataset'
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/forecast', methods=['GET', 'POST'])
 def forecast():
@@ -24,26 +34,34 @@ def forecast():
 
         # Return the prediction as JSON
         return jsonify({'prediction': prediction})
-    
     elif request.method == 'POST':
-        input_data = request.get_json()
+        if 'file' not in request.files:
+            flash('No file part')
+            return jsonify({'error': 'No file uploaded'})
 
-        # Convert input_data to pandas dataframes
-        revenue_input = pd.DataFrame(input_data['revenue_data'])
-        weather_input = pd.DataFrame(input_data['weather_data'])
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return jsonify({'error': 'No selected file'})
 
-        # Convert date columns to datetime format
-        revenue_input['Date'] = pd.to_datetime(revenue_input['Date'])
-        weather_input['dt'] = pd.to_datetime(weather_input['dt'])
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            input_data_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-        # Preprocess the data
-        preprocessed_data = preprocess_data(revenue_data.append(revenue_input), weather_data.append(weather_input))
+            revenue_input = pd.read_excel(input_data_path, sheet_name='November_Revenue', parse_dates=['Date'], engine='openpyxl')
+            weather_input = pd.read_excel(input_data_path, sheet_name='November_Weather', parse_dates=['dt'], engine='openpyxl')
 
-        # Make a prediction
-        prediction = predict_next_month(model, preprocessed_data)
+            # Preprocess the data
+            preprocessed_data = preprocess_data(revenue_data.append(revenue_input), weather_data.append(weather_input))
 
-        # Return the prediction as JSON
-        return jsonify({'prediction': prediction})
+            # Make a prediction
+            prediction = predict_next_month(model, preprocessed_data)
+
+            # Return the prediction as JSON
+            return jsonify({'prediction': prediction})
+        else:
+            return jsonify({'error': 'Invalid file format'})
 
 if __name__ == '__main__':
     app.run(debug=True)
